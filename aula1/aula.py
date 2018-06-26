7c3fef62d91cebdb20ca2316449cc86a486b24e3
root <esp.cob@gmail.com>
2018-06-25 21:59:36 -0300

    aula1

---
+++ aula1/aula.py
@@ -0,0 +1,14 @@
+aluno = (input("Digite o nome do aluno: "))
+
+nota1 = int(input("Digite a nota 1 do aluno: "))
+
+nota2 = int(input("Digite a nota 2  do aluno: "))
+
+media = (nota1 + nota2)/2
+
+if media >= 5:
+    print(" Passou filho da puta")
+else:
+    print(" tomou na jabiraca")
+
+    print("O Aluno %d tem a média %c ".format(aluno, media))    
---
+++ aula1/media2.py
@@ -0,0 +1,6 @@
+aluno = input("Digite o nome do aluno: "))
+nota1 = int(input("Digite a Nota 1: "))
+nota2 = int(input("Digite a Nota 2: "))
+media = (nota1+nota2)/2
+
+print("O aluno {} ficou com {} de média",aluno ,media)
